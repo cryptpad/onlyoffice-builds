d2253fc1f2138881e379320e2718d05fb15ea7fd
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-04-16 14:31:17 +0200

    Update build.sh so it can build from local paths

--- build.sh
+++ build.sh
@@ -1,24 +1,58 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -euxo pipefail
 
 BRANCH=cp7.3.3.60
 
-WORK_DIR=`mktemp -d -t build-oo.XXXXX`
+WORK_DIR=$(mktemp -d -t build-oo.XXXXX)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUT_DIR=$SCRIPT_DIR
 
-cd $WORK_DIR
+cd "$WORK_DIR"
 
-git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/sdkjs
-git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/web-apps
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o|--out)
+      OUT_DIR=$2
+      shift
+      shift
+      ;;
+    -s|--sdkjs-dir)
+      SDKJS_DIR=$2
+      shift
+      shift
+      ;;
+    -w|--web-apps-dir)
+      WEB_APPS_DIR=$2
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown parameter $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ ${SDKJS_DIR+x} ] ; then
+  cp -r "$SDKJS_DIR" .
+else
+  git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/sdkjs
+fi
+
+if [ ${WEB_APPS_DIR+x} ] ; then
+  cp -r "$WEB_APPS_DIR" .
+else
+  git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/web-apps
+fi
 
 cd sdkjs
 make
 
-cd $SCRIPT_DIR
+cd "$OUT_DIR"
 rm -rf sdkjs web-apps
 
 cp -r \
-  $WORK_DIR/sdkjs/deploy/sdkjs \
-  $WORK_DIR/sdkjs/deploy/web-apps \
-  $SCRIPT_DIR
+  "$WORK_DIR/sdkjs/deploy/sdkjs" \
+  "$WORK_DIR/sdkjs/deploy/web-apps" \
+  "$OUT_DIR"
