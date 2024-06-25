053d73e49a3c7a232c121b6ee979bbca2ff8adee
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-06-25 17:04:58 +0200

    Add onlyoffice-editor wrapper

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -euxo pipefail
 
-BRANCH=cp7.3.3.60-api
+OO_BRANCH=cp7.3.3.60-api
+EDITOR_BRANCH=main  # TODO use version here
 
 WORK_DIR=$(mktemp -d -t build-oo.XXXXX)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -37,6 +38,11 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    -e|--onlyoffice-editor-dir)
+      ONLYOFFICE_EDITOR_DIR=$2
+      shift
+      shift
+      ;;
     *)
       echo "Unknown parameter $1"
       exit 1
@@ -47,17 +53,29 @@ done
 if [ ${SDKJS_DIR+x} ] ; then
   copy_git "$SDKJS_DIR" "$WORK_DIR/sdkjs"
 else
-  git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/sdkjs
+  git clone --depth 1 --branch $OO_BRANCH https://github.com/cryptpad/sdkjs.git
 fi
 
 if [ ${WEB_APPS_DIR+x} ] ; then
   copy_git "$WEB_APPS_DIR" "$WORK_DIR/web-apps"
 else
-  git clone --depth 1 --branch $BRANCH https://github.com/cryptpad/web-apps
+  git clone --depth 1 --branch $OO_BRANCH https://github.com/cryptpad/web-apps.git
+fi
+
+if [ ${ONLYOFFICE_EDITOR_DIR+x} ] ; then
+  copy_git "$ONLYOFFICE_EDITOR_DIR" "$WORK_DIR/onlyoffice-editor"
+else
+  git clone --depth 1 --branch "$EDITOR_BRANCH" https://github.com/cryptpad/onlyoffice-editor.git
 fi
 
 cd sdkjs
 make
+
+cd "$WORK_DIR/onlyoffice-editor"
+npm ci
+npm run build
+mv "$WORK_DIR/sdkjs/deploy/web-apps/apps/api/documents/api.js" "$WORK_DIR/sdkjs/deploy/web-apps/apps/api/documents/api-orig.js"
+cp "$WORK_DIR/onlyoffice-editor/dist/api.js" "$WORK_DIR/sdkjs/deploy/web-apps/apps/api/documents/api.js"
 
 cd "$OUT_DIR"
 rm -rf sdkjs web-apps
@@ -66,3 +84,5 @@ cp -r \
   "$WORK_DIR/sdkjs/deploy/sdkjs" \
   "$WORK_DIR/sdkjs/deploy/web-apps" \
   "$OUT_DIR"
+
+ls "$OUT_DIR/web-apps/apps/api/documents/"
